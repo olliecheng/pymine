792bb506325436232cfab6de93c1ca33255bb6ac
Ollie Cheng <denosawr@vivaldi.net>
2020-10-03 11:00:35 +0800

    wait_for_player_movement

--- client/pymine/events.py
+++ client/pymine/events.py
@@ -1,5 +1,5 @@
 import sys
-from .datatypes import Target, Pos
+from .datatypes import Target, Position
 from .data import mobs, items, blocks
 from .exceptions import EventTimeout
 
@@ -11,6 +11,8 @@ import inspect
 import asyncio
 import websockets
 import uuid
+
+from typing import Optional
 
 
 async def async_wait_for_event(
@@ -57,7 +59,15 @@ async def async_wait_for_event(
                 if response["body"].get("eventName", "") != event_name:
                     continue
 
-                elif filter_func(response):
+                if inspect.iscoroutinefunction(filter_func):
+                    # if is a coroutine
+                    filter_result = await filter_func(response["body"])
+                else:
+                    filter_result = await asyncio.get_running_loop().run_in_executor(
+                        None, filter_func, response["body"]
+                    )
+
+                if filter_result:
                     await complete()
                     return response
 
@@ -67,4 +77,32 @@ async def async_wait_for_event(
 
 
 def wait_for_event(event_name: str, filter_func=lambda x: True, timeout: int = 0):
-    return asyncio.run(async_wait_for_event(event_name, filter_func, timeout))
+    return asyncio.get_event_loop().run_until_complete(
+        async_wait_for_event(event_name, filter_func, timeout)
+    )
+
+
+# def wait_for_player_movement(position: Optional[Position] = None):
+def wait_for_player_movement(
+    check_x: Optional[int] = None,
+    check_y: Optional[int] = None,
+    check_z: Optional[int] = None,
+    check_radius: int = 1,
+):
+    def filter_func(r):
+        nonlocal check_x, check_y, check_z
+
+        for dim in ("x", "y", "z"):
+            check: int = locals()[f"check_{dim}"]
+
+            if (
+                check != None
+                and abs(r["measurements"][f"PosAvg{dim.upper()}"] - check)
+                > check_radius
+            ):
+                # print("nope", r, check)
+                return False
+        return True
+
+    wait_for_event("PlayerTravelled", filter_func)
+
