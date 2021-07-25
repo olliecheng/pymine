b6b7c5a50492852a3b57b8a31f690a76f6447a8e
Ollie Cheng <denosawr@vivaldi.net>
2021-07-25 21:49:22 +0800

    Implement freeze
    with MSI file creation!

--- server/freeze_setup.py
+++ server/freeze_setup.py
@@ -1,14 +1,52 @@
 import sys
 from cx_Freeze import setup, Executable
 
-build_exe_options = {}
-
+build_exe_options = {
+    "packages": ["pymine_server", "pystray._win32"],
+    "include_msvcr": True,
+}
 base = "Win32GUI"
 
+msi_data = {
+    "ProgId": [
+        (
+            "denosawr.pymine_server",
+            None,
+            None,
+            "Server exposing a programmatic interface to Minecraft: Education Edition",
+            "mainIcon",
+            None,
+        ),
+    ],
+    "Icon": [("mainIcon", "pymine_server/resources/icon.ico"),],
+}
+
+bdist_msi_options = {
+    "add_to_path": True,
+    "data": msi_data,
+    "upgrade_code": "{e5c7572f-d727-4fa8-ab68-8ae95827f3ed}",
+    "summary_data": {
+        "author": "Ollie Cheng",
+        "comments": "Server exposing a programmatic interface to Minecraft: Education Edition",
+    },
+    "install_icon": "pymine_server/resources/icon.ico",
+}
+
 setup(
-    name="poetry-server",
+    name="pymine-server",
     version="0.1",
     description="Server exposing a programmatic interface to Minecraft: Education Edition",
-    options={"build_exe": build_exe_options},
-    executables=[Executable("__main__.py", base=base)],
+    options={"build_exe": build_exe_options, "bdist_msi": bdist_msi_options},
+    executables=[
+        Executable(
+            "run.py",
+            base=base,
+            target_name="pymine-server",
+            shortcut_name="Pymine Server",
+            shortcut_dir="ProgramMenuFolder",
+            copyright="https://github.com/denosawr/pymine",
+            icon="pymine_server/resources/icon.ico",
+        )
+    ],
+    silent=True,
 )
---
+++ server/run.py
@@ -0,0 +1,3 @@
+from pymine_server import run_program
+
+run_program()
